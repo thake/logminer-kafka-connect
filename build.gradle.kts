1d555512bdc342605fa726eb93996f6dc384bd17
Thorsten Hake <mail@thorsten-hake.com>
2020-09-24 09:47:51 +0200

    Add mockk as test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     testImplementation("org.testcontainers:oracle-xe")
     testImplementation("ch.qos.logback", "logback-classic", "1.2.3")
     testImplementation("ch.qos.logback", "logback-core", "1.2.3")
-
+    testImplementation("io.mockk:mockk:1.10.0")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
